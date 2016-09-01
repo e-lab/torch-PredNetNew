284aecf9ef792a1bc12da4d2315cf24f93af9533
Eugenio Culurciello <euge@purdue.edu>
2016-09-01 15:17:05 -0400

    1 layer working with test

--- model.lua
+++ model.lua
@@ -5,7 +5,6 @@
 
 require 'nn'
 require 'nngraph'
--- require 'cunn'
 
 torch.setdefaulttensortype('torch.FloatTensor')
 nngraph.setDebug(true)
@@ -19,64 +18,67 @@ mapss = {3, 32, 64, 128, 256} -- layer maps sizes
 layer={}
 -- P = prediction branch, A_hat in paper
 
-nlayers = 2
-
--- sizes = {}
--- -- initialize size:
--- for L = 1, nlayers do
---    sizes[L] = {mapss[L], insize/(poolsize^(L-1)), insize/(poolsize^(L-1))} -- sizes of inputs at each layer
--- end
+nlayers = 1
 
 -- define all layers function:
 for L = 1, nlayers do
-   cA = nn.SpatialConvolution(mapss[L], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- A convolution, maxpooling
-   cR = nn.SpatialConvolution(mapss[L+1], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- recurrent
-   cP = nn.SpatialConvolution(mapss[L+1], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- P convolution
-   mA = nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize)
-   up = nn.SpatialUpSamplingNearest(poolsize)
 end
 
-input = nn.Identity()()
-inputs = {}
-outputs = {}
+local input = nn.Identity()()
+local inputs = {}
+local outputs = {}
 table.insert(inputs, nn.Identity()()) -- input image x
 for L = 1, nlayers do
-   table.insert(inputs, nn.Identity()()) -- previous E
+   if L > 1 then table.insert(inputs, nn.Identity()()) end-- previous E
    table.insert(inputs, nn.Identity()()) -- next R
 end
 
-for L = 0, nlayers do
+for L = 1, nlayers do
    print('Creating layer:', L)
-   if L == 0 then
+
+   -- define layer functions:
+   local cA = nn.SpatialConvolution(mapss[L], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- A convolution, maxpooling
+   local cR = nn.SpatialConvolution(mapss[L], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- recurrent
+   local cP = nn.SpatialConvolution(mapss[L+1], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- P convolution
+   local mA = nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize)
+   -- local up = nn.SpatialUpSamplingNearest(poolsize)
+   local op = nn.PReLU(mapss[L+1])
+
+   if L == 1 then
       pE = inputs[1]
    else
-      pE = inputs[2*L] -- previous E
+      pE = inputs[2*L+1] -- previous E
    end
    pE:annotate{graphAttributes = {color = 'green', fontcolor = 'green'}}
    A = pE - cA - mA - nn.ReLU()
    
-   if L >= 1 then
-      nR = inputs[2*L+1] -- next R
-      upR = {nR} - up
-      R = {E, upR} - nn.CAddTable(1) - cR
-      P = {R} - cP - nn.ReLU()
+   nR = inputs[2*L] -- next R
+   -- upR = {nR} - up
+   if L == 1 then
+      R = nR
+   else
+      R = {E, nR} - nn.CAddTable() - cR
    end
-   E = {A, P} - nn.CSubTable() - nn.PReLU(mapss[L+1]) -- PReLU instead of +/-ReLU
+   P = {R} - cP - nn.ReLU()
+   E = {A, P} - nn.CSubTable() - op -- PReLU instead of +/-ReLU
    E:annotate{graphAttributes = {color = 'blue', fontcolor = 'blue'}}
    table.insert(outputs, E)
    table.insert(outputs, R)
 end
-
+-- create graph
 model = nn.gModule(inputs, outputs)
 nngraph.annotateNodes()
 graph.dot(model.fg, 'MatchNet','Model') -- graph the model!
 
+
 -- test:
+testx = {}
+table.insert(testx, torch.Tensor(mapss[1], insize, insize)) -- input
 for L = 1, nlayers do
-   -- x = torch.Tensor(mapss[L], insize, insize) -- input
-   -- y = torch.zeros(mapss[L+1], insize / poolsize, insize / poolsize) -- Rn and Ef
-   -- out = layer[L]:forward({x,y,y,y})
-   -- -- graph.dot(model.fg, 'MatchNet-model','Model') -- graph the model!
-   -- print('output size:', out:size())
-   -- insize = insize/2
+   if L > 1 then table.insert(testx, torch.zeros(mapss[L], insize / poolsize, insize / poolsize)) end-- previous E
+   table.insert(testx, torch.zeros(mapss[L+1], insize / poolsize, insize / poolsize)) -- next R
 end
+out = model:forward(testx)
+   -- -- graph.dot(model.fg, 'MatchNet-model','Model') -- graph the model!
+-- print('output size:', out:size())
+print('output:', out)
