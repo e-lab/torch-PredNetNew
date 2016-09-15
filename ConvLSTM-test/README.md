57ee76518358cd146be146030e9ee107bfdd00b7
Eugenio <culurciello@gmail.com>
2016-09-15 11:52:13 -0400

    convLSTM test working
    learning future frames of video-MNIST with a small dataset or larger
    dataset

---
+++ ConvLSTM-test/README.md
@@ -0,0 +1,11 @@
+# convLSTM test
+
+Simplified code and test from: https://github.com/viorik/ConvLSTM
+
+To run:
+
+1: Install rnn (Element Research) and other required packages
+
+2: Download moving MNIST examples from: https://www.dropbox.com/sh/fvsqod4uv7yp0dp/AAAHoHUjkXg4mW6OvV91TgaEa (small is a 100-sample test, otherwise use the larger ones)
+
+3: run: ```qlua main-mnist.lua```
---
+++ ConvLSTM-test/opts-mnist.lua
@@ -0,0 +1,43 @@
+opt = {}
+-- general options:
+opt.dir     = 'outputs_mnist_line' -- subdirectory to save experiments in
+opt.seed    = 1250         -- initial random seed
+
+-- Model parameters:
+opt.inputSizeW = 64   -- width of each input patch or image
+opt.inputSizeH = 64   -- width of each input patch or image
+opt.eta       = 1e-4 -- learning rate
+opt.etaDecay  = 1e-5 -- learning rate decay
+opt.momentum  = 0.9  -- gradient momentum
+opt.maxIter   = 30000 --max number of updates
+opt.nSeq      = 19
+opt.transf    = 2       -- number of parameters for transformation; 6 for affine or 3 for 2D transformation
+opt.nFilters  = {1,32}--9,45} -- number of filters in the encoding/decoding layers
+opt.nFiltersMemory   = {32,32}--45} --{45,60}
+opt.kernelSize       = 7 -- size of kernels in encoder/decoder layers
+opt.kernelSizeMemory = 7
+opt.kernelSizeFlow   = 15
+opt.padding   = torch.floor(opt.kernelSize/2) -- pad input before convolutions
+opt.dmin = -0.5
+opt.dmax = 0.5
+opt.gradClip = 50
+opt.stride = 1 --opt.kernelSizeMemory -- no overlap
+opt.constrWeight = {0,1,0.001}
+
+opt.memorySizeW = 32
+opt.memorySizeH = 32
+
+opt.dataFile = 'data-small-train.t7'
+opt.dataFileTest = 'data-small-test.t7'
+opt.modelFile = nil
+opt.configFile = nil
+opt.statInterval = 50 -- interval for printing error
+opt.v            = false  -- be verbose
+opt.display      = true -- display stuff
+opt.displayInterval = opt.statInterval
+opt.save         = true -- save models
+opt.saveInterval = 10000
+
+if not paths.dirp(opt.dir) then
+   os.execute('mkdir -p ' .. opt.dir)
+end
