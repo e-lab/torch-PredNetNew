4fb25074ede29e1f824f3bab7a276512baef067d
Eugenio <culurciello@gmail.com>
2016-09-19 13:27:40 -0400

    match net module and tester added

---
+++ ConvLSTM-test/MatchNet-tester.lua
@@ -0,0 +1,42 @@
+-- Eugenio Culurciello
+-- August 2016
+-- MatchNet: a model of PredNet from: https://arxiv.org/abs/1605.08104
+
+require 'nn'
+require 'nngraph'
+require 'MatchNet'
+
+local c = require 'trepl.colorize'
+-- torch.setdefaulttensortype('torch.FloatTensor')
+nngraph.setDebug(true)
+
+
+local insize = 64
+local nlayers = 1
+local input_stride = 1
+local poolsize = 2
+local mapss = {3, 32, 64, 128, 256}
+local clOpt = {}
+clOpt['nSeq'] = 19
+clOpt['stride'] = 1
+
+-- instantiate MatchNet:
+local model = mNet(nlayers, input_stride, poolsize, mapss, clOpt, true)
+nngraph.annotateNodes()
+-- graph.dot(model.fg, 'MatchNet','Model') -- graph the model!
+
+-- test:
+print('Testing model:')
+-- model:cuda()
+
+-- local nT = 1 -- time sequence length
+local inTable = {}
+-- local outTable = {}
+for L = 1, nlayers do
+   table.insert( inTable, torch.ones( mapss[L], insize/2^(L-1), insize/2^(L-1)) ) -- prev E
+   table.insert( inTable, torch.zeros( mapss[L+1], insize/2^(L), insize/2^(L)) ) -- this E
+   if L < nlayers then table.insert( inTable, torch.zeros(mapss[L+1], insize/2^(L+1), insize/2^(L+1)) ) end -- next R
+end
+local outTable = model:forward(inTable)
+print(outTable)
+graph.dot(model.fg, 'MatchNet','Model') -- graph the model!
--- ConvLSTM-test/MatchNet.lua
+++ ConvLSTM-test/MatchNet.lua
@@ -8,8 +8,10 @@ require 'UntiedConvLSTM'
 
 nngraph.setDebug(true)
 
-function mNet(nlayers, input_stride, poolsize, mapss, clOpt)
-   
+function mNet(nlayers, input_stride, poolsize, mapss, clOpt, testing)
+
+   local pE, A, upR, R, P, E, cR
+
    -- P = prediction branch, A_hat in paper
    -- This module creates the MatchNet network model, defined as:
    -- inputs = {prevE, thisE, nextR}
@@ -18,11 +20,12 @@ function mNet(nlayers, input_stride, poolsize, mapss, clOpt)
    -- creating input and output lists:
    local inputs = {}
    local outputs = {}
-   inputs[1] = nn.Identity()() -- previous R
    for L = 1, nlayers do
-      inputs[L+1] = nn.Identity()() -- previous R
+      inputs[3*L-2] = nn.Identity()() -- previous E
+      inputs[3*L-1] = nn.Identity()() -- this E
+      if L < nlayers then inputs[3*L] = nn.Identity()() end -- next R
    end
-
+   
    for L = 1, nlayers do
       print('MatchNet model: creating layer:', L)
 
@@ -34,26 +37,32 @@ function mNet(nlayers, input_stride, poolsize, mapss, clOpt)
       local up = nn.SpatialUpSamplingNearest(poolsize)
       local op = nn.PReLU(mapss[L+1])
       -- recurrent branch:
-      local cR = nn.ConvLSTM(mapss[L],mapss[L+1], clOpt.nSeq, 3, 3, clOpt.stride)
-      -- local cR = nn.Sequencer(nn.ConvLSTM(mapss[L],mapss[L+1], clOpt.nSeq, 3, 3, clOpt.stride))
-
-      local pE, A, upR, P, E
-
-      if L == 1 then
-         pE = inputs[1] -- previous layer E
+      if testing then
+         cR = nn.SpatialConvolution(mapss[L+1], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- recurrent / convLSTM temp model
       else
-         pE = outputs[L-1] 
+         cR = nn.ConvLSTM(mapss[L],mapss[L+1], clOpt.nSeq, 3, 3, clOpt.stride)
+         -- cR = nn.Sequencer(nn.ConvLSTM(mapss[L],mapss[L+1], clOpt.nSeq, 3, 3, clOpt.stride))
       end
-      pE:annotate{graphAttributes = {color = 'green', fontcolor = 'green'}}
-      A = pE - cA - mA - nn.ReLU() - up
-      --iR is already updated so we do second forloop
-      iR = inputs[L+1] 
-      -- iR:annotate{graphAttributes = {color = 'blue', fontcolor = 'green'}}
-      P = iR - cP - nn.ReLU()
+
+      pE = inputs[3*L-2] -- previous layer E
+      if testing then pE:annotate{graphAttributes = {color = 'green', fontcolor = 'green'}} end
+      A = pE - cA - mA - nn.ReLU()
+
+      if L == nlayers then
+         print(inputs[3*L-1], cR)
+         R = inputs[3*L-1] - cR -- this E = inputs[3*L-1] in this layer!
+      else
+         upR = inputs[3*L] - up -- upsampling of next layer R
+         R = {inputs[3*L-1], upR} - nn.CAddTable(1) - cR -- this E = inputs[3*L-1] in this layer!
+      end
+      if testing then R:annotate{graphAttributes = {color = 'red', fontcolor = 'red'}} end
+
+      P = {R} - cP - nn.ReLU()
       E = {A, P} - nn.CSubTable(1) - op -- PReLU instead of +/-ReLU
-      -- E:annotate{graphAttributes = {color = 'blue', fontcolor = 'blue'}}
+      E:annotate{graphAttributes = {color = 'blue', fontcolor = 'blue'}}
       -- set outputs:
-      outputs[1] = E -- this layer E
+      outputs[2*L-1] = E -- this layer E
+      outputs[2*L] = R -- this layer R
    end
 
    return nn.gModule(inputs, outputs)
