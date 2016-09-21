da9ae942ea39f9697b11ba48134068ec60712df8
spk <spkim921@gmail.com>
2016-09-21 00:48:45 -0400

    succend seq=0 layer=2 nnGraph Version

--- nnGraph/rtrain.lua
+++ nnGraph/rtrain.lua
@@ -26,8 +26,9 @@ end
 local insize = 64
 local input_stride = 1
 local poolsize = 2
-local mapss2 = {3, 32, 64, 128, 256} -- layer maps sizes
-local mapss = {32, 64, 128, 256 } --  Out put size of lstm
+local channels = {1, 32, 64} -- layer maps sizes
+local lstmCh = {96, 128} --  Out put size of lstm -- last chnel has no R_l+1 concat
+local cellCh = {32, 64} --  Out put size of lstm -- This is same as output channels
 local nlayers = 2
 
 -- Option for lstm
@@ -39,94 +40,87 @@ clOpt['st'] = 1
 clOpt['pa'] = 1
 clOpt['dropOut'] = 0
 clOpt['lm'] = 1
-local clOpt2 = {}
-clOpt2['nSeq'] = 1
-clOpt2['kw'] = 3
-clOpt2['kh'] = 3
-clOpt2['st'] = 1
-clOpt2['pa'] = 1
-clOpt2['dropOut'] = 0
-clOpt2['lm'] = 1
 
 -- create graph
 print('Creating model:')
 local pn = {}
-pn.main  = mNet(nlayers,input_stride,poolsize,mapss2,clOpt)
+pn.main  = mNet(nlayers,input_stride,poolsize,channels,clOpt)
 pn.lstm = {}
 for i = nlayers, 1, -1 do
-   pn.lstm[i] = lstm(mapss[i+1],mapss[i],clOpt)
+   pn.lstm[i] = lstm(lstmCh[i],cellCh[i],clOpt)
 end
 
 shipGPU(pn)
 -- test:
 print('Testing model:')
-x = {}
-x[nlayers] = torch.Tensor(mapss[nlayers+1],32,32):zero():cuda()
+local x,h,c,ht = {},{},{},{}
 -- Init state for top LSTM
 local initState = {}
 for i = nlayers , 1, -1 do
-   local c = torch.Tensor(mapss[i],32,32):zero():cuda()
-   local h = torch.Tensor(mapss[i],32,32):zero():cuda()
-   local ht = {}
-   for i =1, clOpt.lm do
-      table.insert(ht,c:cuda())
-      table.insert(ht,h:cuda())
-   end
    if i == nlayers then
-      initState[i] = {x[i],unpack(ht)}
+      x[i] = torch.Tensor(lstmCh[i],32,32):zero():cuda()
    else
-      initState[i] = {}
-      initState[i][2] = c
-      initState[i][3] = h
+      -- Need to concat Err so leaving space
+      x[i] = torch.Tensor(cellCh[i+1],32,32):zero():cuda()
    end
+   c[i] = torch.Tensor(cellCh[i],32,32):zero()
+   h[i] = c[i]:clone()
+   ht[i] = {}
+   --Here only lstm layer 1
+   table.insert(ht[i],c[i]:cuda())
+   table.insert(ht[i],h[i]:cuda())
+end
+for i = nlayers, 1, -1 do
+   initState[i] = {x[i],unpack(ht[i])}
 end
 print('InitState of lstm')
 print(initState)
 
 --Make model cuda
-
-function updateLSTM(pn,initState)
+local lstmState = {}
+lstmState[1] = initState
+function updateLSTM(pn,lstmState)
    --Update LSTM topDown lstmOut[1] : cell lstmOut[2]: hidden
-   local lstmOut = {}
+   --Top down
+   local lstmOut, E = {}, {}
    for i = nlayers, 1, -1 do
-      print(i)
-      lstmOut[i] = pn.lstm[i]:forward(initState[i])
-      print(lstmOut)
-      if i ~= 1 then
-         x[i-1] = lstmOut[i][2]
-         initState[i-1][1] = x[i-1]
-         print('initState: '..tostring(i))
-         print(initState[i-1])
+      if i == nlayers then
+         lstmOut[i] = pn.lstm[i]:forward(lstmState[1][i])
+      else
+         upR = lstmOut[i+1][2]
+         --Conv channels is 1 step forward since it starts from 1
+         E[i] = torch.zeros(channels[i+1],32,32):cuda()
+         --Fill up input of LSTM channels
+         lstmState[1][i][1] = torch.cat(upR,E[i],1)
       end
    end
-
-   for i, out in ipairs(lstmOut) do
+   for i, out in ipairs(lstmState) do
       print('OutPut of LSTM'..tostring(i))
       print(out)
    end
-   return lstmOut
+   return lstmState
 end
 
 -- local nT = 1 -- time sequence length
 local inTable = {}
 -- local outTable = {}
-function updateR(L, lstmOut)
+function updateR(L, lstmState)
    local rnnI = {}
    for i = 1 , L do
       --Iff lstm is layer 1
-      rnnI[i] = lstmOut[i][2]
+      rnnI[i] = lstmState[1][i][2]
    end
    return rnnI
 end
 
 --Top Down update convLSTM
-local lstmOut = updateLSTM(pn,initState)
+local lstmState = updateLSTM(pn,lstmState)
 --Extract hidden for input of main
-rnnI = updateR(nlayers, lstmOut)
+rnnI = updateR(nlayers, lstmState)
 print('rnnI')
 print(rnnI)
 --Creat in put for main
-table.insert(inTable, torch.ones(mapss2[1], 32, 32):cuda()) -- input Image
+table.insert(inTable, torch.ones(channels[1], 32, 32):cuda()) -- input Image
 for L = 1, nlayers do
    table.insert(inTable, rnnI[L]) -- prev E
 end
