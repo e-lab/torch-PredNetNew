a33bde5da466bb47bdbe1afa61942e28f73a282b
Eugenio <culurciello@gmail.com>
2016-09-23 09:17:36 -0400

    working version 1 layer
    this is with non parametrized matchnet model

--- ConvLSTM-test/MatchNet.lua
+++ ConvLSTM-test/MatchNet.lua
@@ -7,6 +7,13 @@ require 'nngraph'
 -- require 'UntiedConvLSTM'
 local c = require 'trepl.colorize'
 
+
+-- NOTE: having a parametrized layer version is hard to do and to debug.
+-- for now I will use a fixed params version below. 
+--
+-- The code for the parametrized version is after commented out, and only tested for 1-2 layers
+
+
 function mNet(nlayers, input_stride, poolsize, mapss, clOpt, testing)
 
    local pE, A, upR, R, RR, RE, Ah, E, cR, cRR, cRE, cA, mA, cAh, up, op
@@ -27,64 +34,119 @@ function mNet(nlayers, input_stride, poolsize, mapss, clOpt, testing)
       inputs[4*L-1] = nn.Identity()() -- same layer R (from previous time)
       if L < nlayers then inputs[4*L] = nn.Identity()() end -- next R
    end
-   
-   -- generating network layers:
-   for L = 1, nlayers do
-      if testing then print('MatchNet model: creating layer:', L) end 
 
-      pE = inputs[4*L-3] -- previous layer E
-      if testing then pE:annotate{graphAttributes = {color = 'green', fontcolor = 'black'}} end
-      
-      -- A branch:
-      if L == 1 then
-         A = pE
-      else
-         cA = nn.SpatialConvolution(mapss[L-1], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- A convolution, maxpooling
-         mA = nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize)
-         A = pE - cA - mA - nn.ReLU()
-      end
-
-      -- R / recurrent branch:
-      up = nn.SpatialUpSamplingNearest(poolsize)
-      if L == nlayers then
-         cR = nn.SpatialConvolution(mapss[L], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- same layer E
-         RE = {inputs[4*L-2]} - cR -- same layer E
-         R = {RE, inputs[4*L-1]} - nn.CAddTable(1) -- same layer E preocessed +  previous time R
-      elseif L == 1 then
-         cRE = nn.SpatialConvolution(mapss[L], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- same layer E (same dims)
-         cRR = nn.SpatialConvolution(mapss[L+1], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- prev layer R (higher dims)
-         RR = {inputs[4*L]} - up - cRR -- upsampling of next layer R + conv cRR
-         RE = {inputs[4*L-2]} - cRE -- same layer E + conv cRE 
-         R = {RR, RE, inputs[4*L-1]} - nn.CAddTable(1) -- add all R and previous time R
-      else
-         cRE = nn.SpatialConvolution(mapss[L], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- same layer E (same dims)
-         cRR = nn.SpatialConvolution(mapss[L], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- prev layer R (higher dims)
-         RR = {inputs[4*L]} - up - cRR -- upsampling of next layer R + conv cRR
-         RE = {inputs[4*L-2]} - cRE -- same layer E + conv cRE 
-         R = {RR, RE, inputs[4*L-1]} - nn.CAddTable(1) -- add all R and previous time R
-      end
-      if testing then R:annotate{graphAttributes = {color = 'red', fontcolor = 'black'}} end
-
-      -- A-hat branch:
-      if L == 1 then
-         cAh = nn.SpatialConvolution(mapss[L+1], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- Ah convolution
-      else
-         cAh = nn.SpatialConvolution(mapss[L], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- Ah convolution
-      end
+   -- layer 1:
+   if nlayers == 1 then
+      L = 1
+      A = inputs[4*L-3] -- previous layer E
+      cR = nn.SpatialConvolution(mapss[1], mapss[2], 3, 3, input_stride, input_stride, 1, 1) -- same layer E
+      RE = {inputs[4*L-2]} - cR -- same layer E
+      R = {RE, inputs[4*L-1]} - nn.CAddTable(1) -- same layer E processed +  previous time R
+      cAh = nn.SpatialConvolution(mapss[2], mapss[1], 3, 3, input_stride, input_stride, 1, 1) -- Ah convolution
       Ah = {R} - cAh - nn.ReLU()
       op = nn.PReLU(mapss[L])
       E = {A, Ah} - nn.CSubTable(1) - op -- PReLU instead of +/-ReLU
-      if testing then E:annotate{graphAttributes = {color = 'blue', fontcolor = 'black'}} end
-
       -- output list:
       outputs[3*L-2] = E -- this layer E
       outputs[3*L-1] = R -- this layer R
       outputs[3*L] = Ah -- prediction output
-   
    end
 
+   
+   
    local g = nn.gModule(inputs, outputs)
    if testing then nngraph.annotateNodes() end
    return g
 
 end
+
+   
+
+
+
+
+-- this version below is the one with parametrized layers:
+
+
+
+-- function mNet(nlayers, input_stride, poolsize, mapss, clOpt, testing)
+
+--    local pE, A, upR, R, RR, RE, Ah, E, cR, cRR, cRE, cA, mA, cAh, up, op
+
+--    -- Ah = prediction branch, A_hat in paper
+--    -- This module creates the MatchNet network model, defined as:
+--    -- inputs = {prevE, thisE, nextR}
+--    -- outputs = {E , R}, E == discriminator output, R == generator output
+
+--    -- creating input / output list:
+--    local inputs = {}
+--    local outputs = {}
+
+--    -- initializing inputs:
+--    for L = 1, nlayers do
+--       inputs[4*L-3] = nn.Identity()() -- previous layer E / input
+--       inputs[4*L-2] = nn.Identity()() -- same layer E (from previous time)
+--       inputs[4*L-1] = nn.Identity()() -- same layer R (from previous time)
+--       if L < nlayers then inputs[4*L] = nn.Identity()() end -- next R
+--    end
+   
+--    -- generating network layers:
+--    for L = 1, nlayers do
+--       if testing then print('MatchNet model: creating layer:', L) end 
+
+--       pE = inputs[4*L-3] -- previous layer E
+--       if testing then pE:annotate{graphAttributes = {color = 'green', fontcolor = 'black'}} end
+      
+--       -- A branch:
+--       if L == 1 then
+--          A = pE
+--       else
+--          cA = nn.SpatialConvolution(mapss[L-1], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- A convolution, maxpooling
+--          mA = nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize)
+--          A = pE - cA - mA - nn.ReLU()
+--       end
+
+--       -- R / recurrent branch:
+--       up = nn.SpatialUpSamplingNearest(poolsize)
+--       if L == nlayers then
+--          cR = nn.SpatialConvolution(mapss[L], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- same layer E
+--          RE = {inputs[4*L-2]} - cR -- same layer E
+--          R = {RE, inputs[4*L-1]} - nn.CAddTable(1) -- same layer E preocessed +  previous time R
+--       elseif L == 1 then
+--          cRE = nn.SpatialConvolution(mapss[L], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- same layer E (same dims)
+--          cRR = nn.SpatialConvolution(mapss[L+1], mapss[L+1], 3, 3, input_stride, input_stride, 1, 1) -- prev layer R (higher dims)
+--          RR = {inputs[4*L]} - up - cRR -- upsampling of next layer R + conv cRR
+--          RE = {inputs[4*L-2]} - cRE -- same layer E + conv cRE 
+--          R = {RR, RE, inputs[4*L-1]} - nn.CAddTable(1) -- add all R and previous time R
+--       else
+--          cRE = nn.SpatialConvolution(mapss[L], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- same layer E (same dims)
+--          cRR = nn.SpatialConvolution(mapss[L], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- prev layer R (higher dims)
+--          RR = {inputs[4*L]} - up - cRR -- upsampling of next layer R + conv cRR
+--          RE = {inputs[4*L-2]} - cRE -- same layer E + conv cRE 
+--          R = {RR, RE, inputs[4*L-1]} - nn.CAddTable(1) -- add all R and previous time R
+--       end
+--       if testing then R:annotate{graphAttributes = {color = 'red', fontcolor = 'black'}} end
+
+--       -- A-hat branch:
+--       if L == 1 then
+--          cAh = nn.SpatialConvolution(mapss[L+1], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- Ah convolution
+--       else
+--          cAh = nn.SpatialConvolution(mapss[L], mapss[L], 3, 3, input_stride, input_stride, 1, 1) -- Ah convolution
+--       end
+--       Ah = {R} - cAh - nn.ReLU()
+--       op = nn.PReLU(mapss[L])
+--       E = {A, Ah} - nn.CSubTable(1) - op -- PReLU instead of +/-ReLU
+--       if testing then E:annotate{graphAttributes = {color = 'blue', fontcolor = 'black'}} end
+
+--       -- output list:
+--       outputs[3*L-2] = E -- this layer E
+--       outputs[3*L-1] = R -- this layer R
+--       outputs[3*L] = Ah -- prediction output
+   
+--    end
+
+--    local g = nn.gModule(inputs, outputs)
+--    if testing then nngraph.annotateNodes() end
+--    return g
+
+-- end
