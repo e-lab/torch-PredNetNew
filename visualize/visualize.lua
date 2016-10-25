009818fbfdb46820b416aaefc49400d27d4b4500
Abhishek Chaurasia <abhishek.chaurasia29@hotmail.com>
2016-10-24 22:18:52 -0400

    Updated visualization

--- visualize/visualize.lua
+++ visualize/visualize.lua
@@ -84,59 +84,106 @@ end
 local dataset = torch.load(opt.input):float()/255                 -- load MNIST
 local img, imgGPU
 
+-- Display windows: Input/Predicted image, Error, Representation respectively
+local winImg, winError, winR
 
-local function main()
-   for itr = 1, dataset:size(1) do
-      local res = dataset:size(5)
-
-      -- Initial states
-      local H0 = {}
-      H0[3] = torch.zeros(channels[1], res, res)                  -- C1[0]
-      H0[4] = torch.zeros(channels[1], res, res)                  -- H1[0]
-      H0[5] = torch.zeros(2*channels[1], res, res)                -- E1[0]
-
-      for l = 2, L do
-         res = res / 2
-         H0[3*l]   = torch.zeros(channels[l], res, res)           -- C1[0]
-         H0[3*l+1] = torch.zeros(channels[l], res, res)           -- Hl[0]
-         H0[3*l+2] = torch.zeros(2*channels[l], res, res)         -- El[0]
-      end
-      res = res / 2
-      H0[2] = torch.zeros(channels[L+1], res, res)                -- RL+1
-
-      -- Convert states into CudaTensors if device is cuda
-      if opt.dev == 'cuda' then
-         for l = 2, 3*L+2 do
-            H0[l] = H0[l]:cuda()
-         end
-      end
-
-      local seqOfFrames = {}
-      for seq = 1, dataset:size(2) do
-         -- Input frame should be 3 dimensional
-         -- channel x height x width
-         img = dataset[itr][seq]
-
-         if opt.dev == 'cuda' then
-            imgGPU = imgGPU or torch.CudaTensor(img:size())
-            imgGPU:copy(img)
-            img = imgGPU
-            H0[1] = img:cuda()
-         else
-            H0[1] = img
-         end
-
-         local h = model:forward(H0)
-         for l = 2, #h do
-            H0[l+1] = h[l]
-         end
-
-         seqOfFrames[seq] = img:clone()
-         seqOfFrames[seq+dataset:size(2)] = h[1]:clone()
-      end
-      win = image.display{image = seqOfFrames, legend = 'Original Frames / Predicted Frames', nrow = 20, win = win}
-      io.read()
-   end
+winError = {}
+winR = {}
+for l = 1, L do
+   winError[l] = {}
+   winR[l] = {}
 end
 
-main()
+local winE1, winE2, winE3, winR1, winR2, winR3
+
+local check = 0
+for itr = 1, dataset:size(1) do
+   local res = dataset:size(5)
+
+   -- Initial states
+   local H0 = {}
+   H0[3] = torch.zeros(channels[1], res, res)                  -- C1[0]
+   H0[4] = torch.zeros(channels[1], res, res)                  -- H1[0]
+   H0[5] = torch.zeros(2*channels[1], res, res)                -- E1[0]
+
+   for l = 2, L do
+      res = res / 2
+      H0[3*l]   = torch.zeros(channels[l], res, res)           -- C1[0]
+      H0[3*l+1] = torch.zeros(channels[l], res, res)           -- Hl[0]
+      H0[3*l+2] = torch.zeros(2*channels[l], res, res)         -- El[0]
+   end
+   res = res / 2
+   H0[2] = torch.zeros(channels[L+1], res, res)                -- RL+1
+
+   -- Convert states into CudaTensors if device is cuda
+   if opt.dev == 'cuda' then
+      for l = 2, 3*L+2 do
+         H0[l] = H0[l]:cuda()
+      end
+   end
+
+   local seqOfFrames = {}
+   local errorImg = {}
+   local RImg = {}
+
+   for l = 1, L do
+      errorImg[l] = {}
+      RImg[l] = {}
+   end
+
+   for seq = 1, dataset:size(2) do
+      -- Input frame should be 3 dimensional
+      -- channel x height x width
+      img = dataset[itr][seq]
+
+      if opt.dev == 'cuda' then
+         imgGPU = imgGPU or torch.CudaTensor(img:size())
+         imgGPU:copy(img)
+         img = imgGPU
+         H0[1] = img:cuda()
+      else
+         H0[1] = img
+      end
+
+      local h = model:forward(H0)
+      for l = 2, #h do
+         H0[l+1] = h[l]
+      end
+
+      -- Store all the input frames and predictions of one sequence
+      seqOfFrames[seq] = img:clone()
+      seqOfFrames[seq+dataset:size(2)] = h[1]:clone()
+
+      -- Store states for displaying
+      for l = 1, L do
+         -- Gather error maps
+         for maps = 1, h[3*l+1]:size(1) do
+            errorImg[l][maps] = h[3*l+1][maps]:clone()
+         end
+         -- Gather representation maps
+         for maps = 1, h[3*l]:size(1) do
+            RImg[l][maps] = h[3*l][maps]:clone()
+         end
+      end
+      if seq > 1 then
+         l = 1
+         -- winE1 = image.display{image = errorImg[l], legend = 'Error layer ' .. l, nrow = 20, zoom = 2^(l-1), win = winE1}
+         winR1 = image.display{image = RImg[l], legend = 'Representation layer ' .. l, nrow = 20, zoom = 2^(l-1), win = winR1}
+         l = 2
+         -- winE2 = image.display{image = errorImg[l], legend = 'Error layer ' .. l, nrow = 20, zoom = 2^(l-1), win = winE2}
+         winR2 = image.display{image = RImg[l], legend = 'Representation layer ' .. l, nrow = 20, zoom = 2^(l-1), win = winR2}
+         l = 3
+         -- winE3 = image.display{image = errorImg[l], legend = 'Error layer ' .. l, nrow = 20, zoom = 2^(l-1), win = winE3}
+         winR3 = image.display{image = RImg[l], legend = 'Representation layer ' .. l, nrow = 20, zoom = 2^(l-1), win = winR3}
+         if check == 0 then
+            io.read()
+            check = 1
+         end
+         os.execute("sleep " .. 0.1)
+      end
+   end
+   winImg = image.display{image = seqOfFrames,
+                          legend = 'Original Frames / Predicted Frames',
+                          nrow = 20, win = winImg}
+   io.read()
+end
