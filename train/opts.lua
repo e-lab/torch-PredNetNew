57a47fd7cd992602a2af65c1c096696e6f7c4407
Abhishek Chaurasia <abhishek.chaurasia29@hotmail.com>
2016-11-14 17:59:02 -0500

    Create train and test tensors
    - Split data into train and test set
    - Fix indentation
    - Get input image channel info autmatically

--- train/opts.lua
+++ train/opts.lua
@@ -16,12 +16,11 @@ function opts.parse(arg)
    --lstmLayer    (default 1)
 
    ## Dataset
-   --datapath     (default /media/tensor.t7)
-   --tdatapath     (default /media/tensor.t7)
-   --srcCh        (default 1)          number of input Channel
+   --trainData    (default /media/tensor.t7)
+                  path for train data
+   --testData     (default /media/tensor.t7)
+                  path for test data
    --batch        (default 5)          number of batch size
-   ## Train option
-   --test         Test if needed need to define tdatapath
 
    ## Model
    --layers       (default 3)          number of layers in the model
--- utils/videoToTensor.lua
+++ utils/videoToTensor.lua
@@ -4,15 +4,14 @@ require 'xlua'
 -- Input video to be converted into a tensor
 local input = '/media/HDD1/Datasets2/testVideos/highway/highway08.mp4'
 -- Location to save the tensor
-local save = './highway08.t7'
+local saveTrain = './highway08Train.t7'
+local saveTest  = './highway08Test.t7'
+local trainTestRatio = 4                  -- train/test
 
 -- Specify desired height and width of the dataset as well as the sequence length
 local height = 128
 local width = 256
-local seqLength = 10
-
-local camRes = 'QHD'
-local fps = 30
+local seqLength = 5
 
 --------------------------------------------------------------------------------
 -- Initialize class Frame which can be used to read videos/camera
@@ -20,16 +19,12 @@ local frame = assert(require('framevideo'))
 
 -- TODO Get rid of this part
 -- To do so you will have to modify framevideo.lua
+local camRes = 'QHD'
+local fps = 30
+
 local source = {}
 -- switch input sources
-source.res = {
-   HVGA  = {w =  320, h =  240},
-   QHD   = {w =  640, h =  360},
-   VGA   = {w =  640, h =  480},
-   FWVGA = {w =  854, h =  480},
-   HD    = {w = 1280, h =  720},
-   FHD   = {w = 1920, h = 1080},
-}
+source.res = {QHD   = {w =  640, h =  360}}
 source.w = source.res[camRes].w
 source.h = source.res[camRes].h
 source.fps = fps
@@ -38,7 +33,7 @@ source.fps = fps
 frame:init(input, source)
 local nFrames = frame.nFrames()          -- # of total frames in the video
 
-local frameSeq
+local frameSeqTrain, frameSeqTest
 local currentFrame = torch.FloatTensor(1, seqLength, 3, height, width):zero()
 
 local img = frame.forward(img)
@@ -47,34 +42,56 @@ print("If needed then use this value to normalize your dataset after loading.")
 
 --------------------------------------------------------------------------------
 -- Section to convert image into tensor
-local n = 1                -- Counter for progress bar
-local count = 1            -- Counter for how many frames have been added to one sequence
-local check = 0            -- Check if it is the very first seq
+local n = 1                 -- Counter for progress bar
+local count = 1             -- Counter for how many frames have been added to one sequence
+local checkTrain  = 0       -- Check if it is the very first seq for train data
+local checkTest   = 0       -- Check if it is the very first seq for test data
+local switchFlag  = 'train'
+local switchCount = 1
 
 while img do
    xlua.progress(n, nFrames)
-   n = n + 1
 
    currentFrame[1][count] = image.scale(img[1], width, height)
 
    if count == seqLength then
       count = 1
-      if check == 0 then                        -- When it is first seq then just put it in the output tensor
-         check = 1
-         frameSeq = currentFrame:clone()
+      if switchFlag == 'train' then
+         if checkTrain == 0 then                       -- When it is first seq then just put it in the output tensor
+            checkTrain = 1
+            frameSeqTrain = currentFrame:clone()
+         else
+            frameSeqTrain = frameSeqTrain:cat(currentFrame, 1)   -- Concat current seq to the output tensor
+         end
+
+         switchCount = switchCount + 1
+         if switchCount > trainTestRatio then
+            switchFlag = 'test'
+         end
       else
-         frameSeq = frameSeq:cat(currentFrame, 1)   -- Concat current seq to the output tensor
+         if checkTest == 0 then                       -- When it is first seq then just put it in the output tensor
+            checkTest = 1
+            frameSeqTest = currentFrame:clone()
+         else
+            frameSeqTest = frameSeqTest:cat(currentFrame, 1)   -- Concat current seq to the output tensor
+         end
+
+         switchCount = 1
+         switchFlag = 'train'
       end
    else
       count = count + 1
    end
 
    img = frame.forward(img)
+   n = n + 1
 end
 
 print("Conversion from video to tensor completed.")
-print("\n# of chunks created: " .. frameSeq:size(1))
+print("\n# of training chunks created: " .. frameSeqTrain:size(1))
+print("\n# of testing chunks created: " .. frameSeqTest:size(1))
 print("Frame resolution is " .. height .. ' x ' .. width)
-print("\nSaving tensor to location: " .. save)
-torch.save(save, frameSeq)
+print("\nSaving tensor to location: " .. saveTrain)
+torch.save(saveTrain, frameSeqTrain)
+torch.save(saveTest,  frameSeqTest)
 print("Tensor saved successfully!!!")
